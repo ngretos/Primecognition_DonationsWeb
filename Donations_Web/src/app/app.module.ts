d4a7d65aa1412b349d0a793337103b44598f73fe
nikosgr <nikosgr@192.168.1.7>
2020-03-20 19:04:20 +0200

    Adding Material design

--- Donations_Web/src/app/app.module.ts
+++ Donations_Web/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -11,11 +11,15 @@ import { InitialpageComponent } from './initialpage/initialpage.component';
 import { DonationslistComponent } from './show/donationslist/donationslist.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 const appRoutes: Routes = [
   { path: 'add', component: AddComponent },
   { path: 'show', component: ShowComponent },
   { path: 'initial', component: InitialpageComponent },
-  { path: '',
+  {
+    path: '',
     redirectTo: '/initial',
     pathMatch: 'full'
   }
@@ -37,9 +41,12 @@ const appRoutes: Routes = [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
